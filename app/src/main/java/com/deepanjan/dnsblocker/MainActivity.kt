091bb7b7e53ab7cc890614e71f2d6764bb57c507
deepanjan0 <deepanjan0@users.noreply.github.com>
2026-02-10 12:38:08 +0530

    Step-by-step clean build structure

--- app/src/main/java/com/deepanjan/dnsblocker/MainActivity.kt
+++ app/src/main/java/com/deepanjan/dnsblocker/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Build Pipeline Fixed!")
+            Text(text = "Step by Step Build!")
         }
     }
 }
